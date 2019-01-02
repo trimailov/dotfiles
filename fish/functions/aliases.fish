338aa24d6bf304f9340a6e107f8178ccaf327f5d
Justas Trimailovas <j.trimailovas@gmail.com>
2019-01-02 12:23:14 +0200

    Delete most aliases, using abbr instead
    Using `fish_confih`, I've set up abbreviations for most of these
    commands. IMO it's better than alias, as it will expand the abbreviation
    so autocomplete will work normally for commands like `git checkout`,
    `git branch`, etc.
    
    More info:
    https://fishshell.com/docs/current/commands.html#abbr

--- fish/functions/aliases.fish
+++ fish/functions/aliases.fish
@@ -2,38 +2,9 @@
 
 alias ag="ag --path-to-ignore ~/.ignore"
 
-# GIT aliases
-
-alias ga="git add"
-alias gb="git branch"
-alias gba="git branch -a"
-alias gc="git commit -v"
-alias gca="git commit -v -a"
-alias gcmsg="git commit -m"
-alias gco="git checkout"
-alias gd="git diff"
-alias gfap="git fetch --all --prune"
-alias glg="git log"
-alias glgg="git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
-alias gp="git push"
-alias gpf="git push -f"
-alias grba="git rebase --abort"
-alias grbc="git rebase --continue"
-alias grbi="git rebase --interactive"
-alias gst="git status"
-alias gsta="git stash"
-alias gstp="git stash pop"
-alias gup="git pull"
-alias gupr="git pull --rebase"
-
-# VIM aliases
-
 # it's just annoying to accidenatly get into vi
 alias nvi="nvim"
 alias vi="vim"
-
-# TIG aliases
-alias tiga="tig --all"
 
 # Misc.
 alias journal="mkdir -p ~/journal/(date +%Y); vim + ~/journal/(date +%Y)/(date +%Y-%m-%d).md"
