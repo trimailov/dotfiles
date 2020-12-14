1a4d8b1f13e7e22c57ce56e7546d8ac4482fb470
Justas Trimailovas <justas@convious.com>
2020-12-14 11:07:49 +0200

    Link neovim configs

--- setup.sh
+++ setup.sh
@@ -4,3 +4,5 @@ ln -s ~/dotfiles/fish/config.fish ~/.config/fish/config.fish
 ln -s ~/dotfiles/fish/functions ~/.config/fish
 ln -s ~/dotfiles/nvim ~/.config/nvim
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/nvim ~/.vim
+ln -s ~/dotfiles/nvim/init.vim ~/.vimrc
